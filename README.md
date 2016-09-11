81bbb31db23220095c5c8b526626d96c98385eed
WilliamMoU <momanyiwilliam.ny@hotmail.com>
2016-09-12 00:04:43 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # spring-rest-api
 Web Service which exposes an interface for client integration. It would query a database that submits a response on SMS URL
+
+# Introduction
+
+The example uses OAuth2 Authorization framework to control access to a resource brookside-server when accessed by a client application.
+
+[OAuth2](https://tools.ietf.org/html/draft-ietf-oauth-v2-31) is an authorization framework that enables applications to obtain limited access to user accounts on an HTTP service, such as Facebook, GitHub, and Twitter.
+It works by delegating user authentication to the service that hosts the user account, and authorizing third-party applications to access the user account.
+OAuth 2 provides authorization flows for web and desktop applications, and mobile devices.
+
+# Overview
+
+# Resource Server
+Resource Server hosts the resources [our REST API] the client is interested in. Resources are located on /user/.
+
+# Authorization Server
+Authorization server is the one responsible for verifying credentials and if credentials are OK, providing the tokens[refresh-token as well as access-token].
+It also contains information about registered clients and possible access scopes and grant types.
+
+# Security Configuration
+Gluing everything together. Endpoint /oauth/token is used to request a token [access or refresh].
+Resource owners [mou,bob] are configured here itself.
+
+# Endpoints and their purpose
+Attempt to access resources [REST API] without any authorization [will fail of-course].
+GET http://localhost:8080/brookside-spring-example/user/
+Ask for tokens[access+refresh] using HTTP POST on /oauth/token, with grant_type=password,and resource owners credentials as req-params.
+Additionally, send client credentials in Authorization header.
+POST http://localhost:8080/brookside-spring-example/oauth/token?grant_type=password&username=mou&password=mou123
+
+This database example is comprised of the following classes.
+
+* The `UserServiceImpl` illustrates using the [SQL Object Queries](http://jdbi.org/sql_object_api_queries/) and string template
+features in JDBI.
+
+* All the SQL statements for use in the `UserService` are located the .....
+
+* `migrations.xml` ....
+
+# Running The Application
+
+To test the example application run the following commands.
+
+* Setup your sanitizer client example.
+
+    mkdir src/main/resources/assets
+    cp -r ../../client/* src/main/resources/assets
+
+* To package the example run.
+
+        mvn package
+
+* To setup the oracle database run......baadae
+
+
+
+## Running the application
+Run it and test it using two different clients.
+
+### Client 1: Postman
+Try to access a resource without any auth info, wil get a 401.
+
+### Client 2: RestTemplate based java application
+Method sendTokenRequest is used to actually get the tokens. The access-token we got in response is then used with each request.
+If required, You can implement the refresh-token flow easily in below SpringRestClient.java has been used a a minimal GUI client
+that demonstrates security and role based authorization.
+
+
+* To hit this url to access the server example.
+
+	http://localhost:8080/brookside-spring-example/user/
+
+* To post data into the application you have use your login credentials in example.
+
+    http://localhost:8080/brookside-spring-example/oauth/token?grant_type=password&username=bill&password=abc123
+
+    Postman screen is shwo below
+
+    ![Alt text](/relative/path/to/img.jpg?raw=true "Optional Title")
---
+++ src/main/java/com/mou/springmvc/controller/HelloWorldRestController.java
@@ -0,0 +1,136 @@
+package com.mou.springmvc.controller;
+ 
+import java.util.List;
+
+import com.mou.springmvc.model.User;
+import com.mou.springmvc.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+public class HelloWorldRestController {
+
+    protected final transient Log log = LogFactory.getLog(getClass());
+
+    @Autowired
+    UserService userService;  //Service which will do all data retrieval/manipulation work
+ 
+     
+    //-------------------Retrieve All Users--------------------------------------------------------
+     
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> listAllUsers() {
+        List<User> users = userService.findAllUsers();
+        if(users.isEmpty()){
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+ 
+ 
+    //-------------------Retrieve Single User--------------------------------------------------------
+     
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<User> getUser(@PathVariable("id") long id) {
+
+        log.debug("Fetching User with id " + id);
+      //  System.out.println("Fetching User with id " + id);
+
+        User user = userService.findById(id);
+        if (user == null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+ 
+     
+     
+    //-------------------Create a User--------------------------------------------------------
+     
+    @RequestMapping(value = "/user/", method = RequestMethod.POST)
+    public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
+
+        log.debug("Creating User " + user.getName());
+       // System.out.println("Creating User " + user.getName());
+ 
+        if (userService.isUserExist(user)) {
+            System.out.println("A User with name " + user.getName() + " already exist");
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }
+ 
+        userService.saveUser(user);
+ 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+ 
+     
+    //------------------- Update a User --------------------------------------------------------
+     
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        System.out.println("Updating User " + id);
+
+        log.debug("Updating User " + user.getName() +"with id" +id);
+
+        User currentUser = userService.findById(id);
+
+        System.out.println("Updating User " + id);
+        if (currentUser==null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+ 
+        currentUser.setName(user.getName());
+        currentUser.setAge(user.getAge());
+        currentUser.setSalary(user.getSalary());
+         
+        userService.updateUser(currentUser);
+        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
+    }
+ 
+    //------------------- Delete a User --------------------------------------------------------
+     
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting User with id " + id);
+        log.debug("Fetching & Deleting User with id " + id);
+
+        User user = userService.findById(id);
+        if (user == null) {
+            System.out.println("Unable to delete. User with id " + id + " not found");
+
+            log.debug("Unable to delete. User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+ 
+        userService.deleteUserById(id);
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+    }
+ 
+     
+    //------------------- Delete All Users --------------------------------------------------------
+     
+    @RequestMapping(value = "/user/", method = RequestMethod.DELETE)
+    public ResponseEntity<User> deleteAllUsers() {
+        System.out.println("Deleting All Users");
+        log.debug("Deleting All Users");
+
+        userService.deleteAllUsers();
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+    }
+ 
+}
